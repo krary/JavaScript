6e35262c5a4c2b1a52f8034bddff59ee1aa539ef
krary <fraggmentado@gmail.com>
2024-03-31 17:18:48 +0200

    upgrade

--- Mensajeria/servidor.js
+++ Mensajeria/servidor.js
@@ -2,6 +2,15 @@ const express = require('express');
 const http = require('http');
 const app = express();
 const server = http.createServer(app);
+const {Server} = require('socket.io');
+const io = new Server(server);
+
+
+io.on('connection',(socket)=>{
+    socket.on('chat',(msg)=>{
+        console.log(msg)
+    })})
+
 
 app.get('/',(req,res)=>{
 
@@ -9,5 +18,5 @@ var variable_raiz= __dirname+"\\"+"plantilla_2.html";
 res.sendFile(variable_raiz);
 })
 
-server.listen(4000,()=>{console.log("SERVER LOADED")});
+server.listen(4000,()=>{console.log("SERVER LOADING...")});
  
